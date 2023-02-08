a6a8843cdac947747a14cab26419622dad926f1d
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-02-08 08:34:35 +0530

    [COMMIT] [extenders] Minor changes to array functions based on bugs and adding new functions. Adding demos for the Array functions. Adding tests for Array functions and exports of package

--- test/demos.array.js
+++ test/demos.array.js
@@ -6,7 +6,7 @@
  * Install: npm i extenders --save
  * Github: https://github.com/ganeshkbhat/jsextenders
  * npmjs Link: https://www.npmjs.com/package/extenders
- * File: src/demos.array.js
+ * File: Tests for src/.js in test/demos.array.js
  * File Description:
  *
 */
@@ -18,27 +18,24 @@
 
 const expect = require('chai').expect;
 
-describe('test-.mjs::extenders: Test Suite for extenders Files', function() {
+describe('test-.mjs::extenders: Test Suite for extenders Files', function () {
 
-    before(async function(){
+    // before(async function(){
+    // });
 
-    });
-
-
-    describe ('test-.js::extenders: [Test A] Test Suite for extenders in main repo directory', function() {
+    describe('test-.js::extenders: [Test A] Test Suite for extenders in main repo directory', function () {
 
         // it('[Test A] Test for ', function(done){
         //     expect(200).to.equal(200);
         //     done();
         // });
 
-        it('[Test A] Test for ', function(done) {
+        it('[Test A] Test for ', function (done) {
             expect(100).to.equal(100);
             done();
         });
 
     });
-
 
 });
 
--- test/demos.math.js
+++ test/demos.math.js
@@ -1,44 +1,40 @@
-// /**
-//  *
-//  * Package:
-//  * Author: Ganesh B
-//  * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
-//  * Install: npm i extenders --save
-//  * Github: https://github.com/ganeshkbhat/jsextenders
-//  * npmjs Link: https://www.npmjs.com/package/extenders
-//  * File: src/demos.math.js
-//  * File Description:
-//  *
-// */
+/**
+ *
+ * Package:
+ * Author: Ganesh B
+ * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
+ * Install: npm i extenders --save
+ * Github: https://github.com/ganeshkbhat/jsextenders
+ * npmjs Link: https://www.npmjs.com/package/extenders
+ * File: Tests for src/demos.math.js in test/demos.math.js
+ * File Description:
+ *
+*/
 
-// /* eslint no-console: 0 */
+/* eslint no-console: 0 */
 
-// 'use strict';
+'use strict';
 
-//
-// const expect = require('chai').expect;
-//
-// describe('test-.mjs::extenders: Test Suite for extenders Files', function() {
-//
-//     before(async function(){
-//
-//     });
-//
-//
-//     describe ('test-.js::extenders: [Test A] Test Suite for extenders in main repo directory', function() {
-//
-//         // it('[Test A] Test for ', function(done){
-//         //     expect(200).to.equal(200);
-//         //     done();
-//         // });
-//
-//         it('[Test A] Test for ', function(done) {
-//             expect(100).to.equal(100);
-//             done();
-//         });
-//
-//     });
-//
-//
-// });
-//
+
+const expect = require('chai').expect;
+
+describe('test-.mjs::extenders: Test Suite for extenders Files', function () {
+
+
+    describe('test-.js::extenders: [Test A] Test Suite for extenders in main repo directory', function () {
+
+        // it('[Test A] Test for ', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        // it('[Test A] Test for ', function(done) {
+        //     expect(100).to.equal(100);
+        //     done();
+        // });
+
+    });
+
+
+});
+
