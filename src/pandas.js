5fdb365fba65456215aad7e7c87a32e85bfaf495
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-02-27 08:28:42 +0530

    [Commit] 1. Adding tests for array. 2. Adding numpyjs, pandas files for todo. 3. Considering _ missing functions seriously. Adding todo list

---
+++ src/pandas.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Package: extenders
+ * Author: Ganesh B
+ * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
+ * Install: npm i extenders --save
+ * Github: https://github.com/ganeshkbhat/jsextenders
+ * npmjs Link: https://www.npmjs.com/package/extenders
+ * File: src/pandasjs.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+ 
+
+module.exports.Numpy = {};
---
+++ test/demos.numpyjs.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * Package:
+ * Author: Ganesh B
+ * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
+ * Install: npm i extenders --save
+ * Github: https://github.com/ganeshkbhat/jsextenders
+ * npmjs Link: https://www.npmjs.com/package/extenders
+ * File: Tests for src/demos.object.js in test/demos.pandas.js
+ * File Description:
+ *
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+
+const expect = require('chai').expect;
+
+describe('test-.mjs::extenders: Test Suite for extenders Files', function () {
+
+
+    describe('test-.js::extenders: [Test A] Test Suite for extenders in main repo directory', function () {
+
+        // it('[Test A] Test for ', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        // it('[Test A] Test for ', function(done) {
+        //     expect(100).to.equal(100);
+        //     done();
+        // });
+
+    });
+
+
+});
+
