f9ce77b23b53a82fe69e878c70a9b0f595a984b0
Ganesh K Bhat <ganeshsurfs@gmail.com>
2023-04-28 18:57:16 +0530

    [COMMIT] Adding new version with decorators feature

--- test/demos.decorator.method.js
+++ test/demos.decorator.method.js
@@ -16,6 +16,7 @@
 'use strict';
 
 const { expect } = require('chai');
+const sinon = require('sinon');
 const createMethodDecorator = require('./src/decorator').createMethodDecorator;
 
 
@@ -61,4 +62,51 @@ describe('createMethodDecorator', () => {
         expect(result).to.equal(decoratorFn(5, multiplier));
     });
 });
+
+
+describe('createMethodDecorator', () => {
+  it('should return a function that returns a descriptor', () => {
+    const decoratorFn = sinon.spy();
+    const methodDecorator = createMethodDecorator(decoratorFn);
+    const descriptor = methodDecorator({}, 'testMethod', { value: () => {} });
+    expect(descriptor).to.have.keys(['value']);
+    expect(descriptor.value).to.be.a('function');
+  });
+
+  it('should call the decorator function with the result and arguments', () => {
+    const decoratorFn = sinon.spy();
+    const methodDecorator = createMethodDecorator(decoratorFn);
+    const target = {};
+    const methodName = 'testMethod';
+    const descriptor = { value: () => {} };
+    const fnArgs = [1, 2, 3];
+    methodDecorator(target, methodName, descriptor).value(...fnArgs);
+    expect(decoratorFn.calledOnceWithExactly(undefined, 1, 2, 3)).to.be.true;
+  });
+
+  it('should call the original method with the correct arguments', () => {
+    const decoratorFn = sinon.stub().returnsArg(0);
+    const methodDecorator = createMethodDecorator(decoratorFn);
+    const target = {};
+    const methodName = 'testMethod';
+    const originalFn = sinon.spy();
+    const descriptor = { value: originalFn };
+    const fnArgs = [1, 2, 3];
+    methodDecorator(target, methodName, descriptor).value(...fnArgs);
+    expect(originalFn.calledOnceWithExactly(...fnArgs)).to.be.true;
+  });
+
+  it('should return the result of the decorator function', () => {
+    const decoratorFn = sinon.stub().returns('decorated result');
+    const methodDecorator = createMethodDecorator(decoratorFn);
+    const target = {};
+    const methodName = 'testMethod';
+    const originalFn = () => 'original result';
+    const descriptor = { value: originalFn };
+    const fnArgs = [1, 2, 3];
+    const result = methodDecorator(target, methodName, descriptor).value(...fnArgs);
+    expect(result).to.equal('decorated result');
+  });
+});
+
 
