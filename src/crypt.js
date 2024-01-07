d6a8253746fedd77df866c2daeb4cd83e7172e15
Ganesh B <ganeshsurfs@gmail.com>
2024-01-07 08:55:01 +0530

    [COMMIT] Adding mincor changes to package. Adding crypt functions extenders. Adding function extenders.

---
+++ src/crypt.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Package: extenders
+ * Author: Ganesh B
+ * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
+ * Install: npm i extenders --save
+ * Github: https://github.com/ganeshkbhat/jsextenders
+ * npmjs Link: https://www.npmjs.com/package/extenders
+ * File: src/array.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const crypter = require("./cryptobase");
+
+function CryptoExtended() {
+  Object.defineProperty(Function.prototype, 'MapperCopy', { value: MapperCopy, enumerable: true, });
+}
+
+function extendCrypto() {
+  Object.defineProperty(Function.prototype, 'MapperCopy', { value: MapperCopy, enumerable: true, });
+}
+
+
+module.exports.FunctionExtended = FunctionExtended;
+module.exports.extendFunction = extendFunction;
---
+++ src/cryptobase.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ * Package: extenders
+ * Author: Ganesh B
+ * Description: Node module for Array, Object, String, Decorator extention Utils like for Python and more for Node.js
+ * Install: npm i extenders --save
+ * Github: https://github.com/ganeshkbhat/jsextenders
+ * npmjs Link: https://www.npmjs.com/package/extenders
+ * File: src/array.js
+ * File Description: 
+ * 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const cryptor = require("hasher-apis");
+
+function encrypt(data, salt) {
+
+}
+
+function decrypt(hashData, salt) {
+
+}
+
+function encryptWithKey(data, key) {
+
+}
+
+function decryptWithKey(hashData, key) {
+
+}
+
+function encryptWithRSAKey(data, key) {
+
+}
+
+function decryptWithRSAKey(hashData, key) {
+
+}
+
+module.exports.encrypt = encrypt;
+module.exports.decrypt = decrypt;
+module.exports.encryptWithKey = encryptWithKey;
+module.exports.decryptWithKey = decryptWithKey;
+module.exports.encryptWithRSAKey = encryptWithRSAKey;
+module.exports.decryptWithRSAKey = decryptWithRSAKey;
